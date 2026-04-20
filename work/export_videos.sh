2956f0260ecc99aad4db67c02223574f869ce3b6
ljh <Like13664954287@gmail.com>
2026-04-20 11:35:33 +0800

    Feat: eval

---
+++ work/export_videos.sh
@@ -0,0 +1,7 @@
+export LD_LIBRARY_PATH="/meta_eon_cfs/home/lhs/miniconda3/envs/vggt/lib:$LD_LIBRARY_PATH" # 这个egl库用别人的conda环境下的，因为uv好像不能装c库
+export CUDA_VISIBLE_DEVICES=7
+uv run scripts/export_videos.py \
+    task=libero \
+    exp_name=reproduce_libero90_baku_adapt3r_15_videos \
+    variant_name=epoch_0080 \
+    checkpoint_path=experiments/libero/libero_90/reproduce_libero90_baku_adapt3r_15/multitask_model_epoch_0080.pth
